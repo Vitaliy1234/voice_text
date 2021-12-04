4e96f60768f48c13558efe2205b9e0b3bb6e1bf3
Anton <amaslov455@gmail.com>
2021-12-04 23:25:50 +0500

    updated repository

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       home: TextToSpeech(),
       debugShowCheckedModeBanner: false,
     );
@@ -35,6 +39,14 @@ class _TextToSpeechState extends State<TextToSpeech> {
   bool _buttonPressed = false;
   bool _loopActive = false;
 
+  late CameraController _camera;
+  bool _cameraInitialized = false;
+  late CameraImage _savedImage;
+
+  List<CameraDescription> _cameras = [];
+  late CameraController _controller_camera;
+
+
   void initializeTts() {
     _flutterTts.setStartHandler(() {
       setState(() {
@@ -54,13 +66,57 @@ class _TextToSpeechState extends State<TextToSpeech> {
   }
 
   @override
-  void initState() {
-    
-    _flutterTts.setLanguage("ru-RU");
-    _flutterTts.awaitSpeakCompletion(true);
+  void initState() {    
     super.initState();
     initializeTts();
+    _initializeCamera();
+
+    _flutterTts.setLanguage("ru-RU");
+    _flutterTts.awaitSpeakCompletion(true);
+
+
   }
+  // Camera
+  Future<CameraDescription> _getCamera(CameraLensDirection dir) async {
+    return await availableCameras().then(
+      (List<CameraDescription> cameras) => cameras.firstWhere(
+            (CameraDescription camera) => camera.lensDirection == dir,
+          ),
+    );
+  }
+
+  void _initializeCamera() async {
+    _camera = CameraController(await _getCamera(CameraLensDirection.back),ResolutionPreset.medium);
+    _camera.initialize().then((_) async{
+    // Start ImageStream
+    await _camera.startImageStream((CameraImage image) =>
+      _processCameraImage(image));
+      setState(() {
+        _cameraInitialized = true;
+      });
+  });
+    // _camera.startImageStream((CameraImage image) {
+    //   if (_isDetecting) return;
+    //   _isDetecting = true;
+    //   try {
+    //     // await doSomethingWith(image)
+    //   } catch (e) {
+    //     // await handleExepction(e)
+    //   } finally {
+    //     _isDetecting = false;
+    //   }
+    // });
+    setState(() {
+      
+    });
+  }
+
+  void _processCameraImage(CameraImage image) async {
+  setState(() {
+    print('image saved');
+    _savedImage = image;
+  });
+}
 
   void speakWhilePressed() async {
     if (_loopActive) return;
@@ -69,11 +125,11 @@ class _TextToSpeechState extends State<TextToSpeech> {
       if (!isSpeaking){
         if (_controller.text.isNotEmpty) {
           print(isSpeaking);
+          // var text_to_speech = 
           await _flutterTts.speak(_controller.text);
           await Future.delayed(Duration(milliseconds: _controller.text.length * 100), () {
             
           });
-          print(_controller.text.length);
         }
       }
     }
@@ -102,21 +158,32 @@ class _TextToSpeechState extends State<TextToSpeech> {
     super.dispose();
   }
 
+  void imageToText(CameraImage img) async {
+    String to_speech;
+    to_speech = await pythonScriptRun(img); //Питон скрипт
+    await _flutterTts.speak(to_speech);
+    setState(() {
+      _buttonPressed = false;
+    });
+  }
+
+  String pythonScriptRun(CameraImage img){
+    return 'на картинке аниме';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Text To Speech"),
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 40,
-            width: double.infinity,
-            child: TextField(
-              controller: _controller,
-            ),
-          ),
+      body: Center(
+          child:
+            (_cameraInitialized)
+            ? AspectRatio(aspectRatio: _camera.value.aspectRatio,
+                child: CameraPreview(_camera),)
+            : CircularProgressIndicator()
+        ),
           // Listener(
           //     onPointerDown: (details) {
           //       _buttonPressed = true;
@@ -130,21 +197,24 @@ class _TextToSpeechState extends State<TextToSpeech> {
           //       padding: EdgeInsets.all(16.0),
           //       child: Text('Anime'),
             // ),
-            ElevatedButton(
+            floatingActionButton: FloatingActionButton.extended(
+              label: Text(_buttonPressed ? "Остановаить" : "Произнести"),
+              icon: Icon(const IconData(61309, fontFamily: 'MaterialIcons')),
               onPressed: () async {
                 if (_buttonPressed){
-                  _buttonPressed = false;
-                  stop();
+                  // _buttonPressed = false;
+                  // stop();
+                  return null;
                 }else{
-                  print('anime');
-                  _buttonPressed = true;
-                  speakWhilePressed();
+                  setState(() {
+                    _buttonPressed = true;
+                    imageToText(_savedImage);
+                  });
+                  // speakWhilePressed();
                 }
               },
-              child: Text(_buttonPressed ? "Остановить" : "Произносить"),
           ),
-        ],
-      ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
